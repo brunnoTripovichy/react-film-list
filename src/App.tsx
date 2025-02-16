561d58156399529b7c3d0365d71415b1f5da50c0
Brunno Tripovichy <brunno.tripovichy@gmail.com>
2025-02-16 13:25:17 -0500

    api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import MovieList from './features/MovieList';
 import NavBar from './features/NavBar';
@@ -10,24 +10,73 @@ import ListBox from './components/ListBox';
 import Movie from './components/Movie';
 import WatchedSummary from './features/WatchedSummary';
 import WatchedList from './features/WatchedList';
+import Loader from './components/Loader';
 
 const App = () => {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [selectedId, setSelectedId] = useState('');
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        setIsLoading(true);
+        setError('');
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=4663f48c&s=${searchTerm}`,
+        );
+
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const data = await res.json();
+
+        if (data.Response === 'False') {
+          throw new Error(data.Error);
+        }
+
+        setMovies(data.Search);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setError(error.message);
+        setIsLoading(false);
+      }
+    };
+
+    if (searchTerm.trim() === '' || searchTerm.length < 3) {
+      setMovies([]);
+      setError('');
+      return;
+    }
+
+    fetchMovies();
+  }, [searchTerm]);
+
   return (
     <div>
       <NavBar>
         <AppLogo />
-        <InputSearch />
+        <InputSearch searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         <SearchResults movies={movies} />
       </NavBar>
       <MovieList>
         <ListBox>
-          {movies?.map((movie, index) => (
-            <ul className="list" key={index}>
-              {<Movie movie={movie} />}
-            </ul>
-          ))}
+          {!isLoading &&
+            !error &&
+            movies?.map((movie, index) => (
+              <ul className="list" key={index}>
+                {<Movie movie={movie} />}
+              </ul>
+            ))}
+
+          {isLoading && <Loader />}
+
+          {error && <p>{error}</p>}
         </ListBox>
         <ListBox>
           <WatchedSummary watched={watched} />
--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -1,14 +1,19 @@
-import { useState } from 'react';
+interface InputSearchProps {
+  searchTerm: string;
+  setSearchTerm: (searchTerm: string) => void;
+}
 
-const InputSearch = () => {
-  const [query, setQuery] = useState('');
+const InputSearch: React.FC<InputSearchProps> = ({
+  searchTerm,
+  setSearchTerm,
+}) => {
   return (
     <input
       className="search"
       type="text"
       placeholder="Search movies..."
-      value={query}
-      onChange={(e) => setQuery(e.target.value)}
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
     />
   );
 };
